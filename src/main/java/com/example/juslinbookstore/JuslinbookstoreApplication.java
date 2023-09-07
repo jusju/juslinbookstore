04a13a7de70a54569e32f91c843a66de7a798831
Juslin Jukka <jusju@haaga-helia.fi>
2023-09-07 08:38:53 +0300

    first commit

---
+++ src/main/java/com/example/juslinbookstore/JuslinbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.juslinbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuslinbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuslinbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juslinbookstore/JuslinbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juslinbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuslinbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
